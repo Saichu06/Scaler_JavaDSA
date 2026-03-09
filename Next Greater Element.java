b421d1685bc84ed2ec12f8389878ad6477987bfc
Saichu <saichuwork@gmail.com>
2026-03-09 08:42:33 +0530

    Next Greater Element

---
+++ Next Greater Element.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> st=new Stack<>();
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int num:nums2){
+            while(!st.isEmpty() && st.peek()<num){
+                map.put(st.pop(),num);
+            }
+            st.push(num);
+        }
+        for(int i=0;i<nums1.length;i++){
+            nums1[i]=map.getOrDefault(nums1[i],-1);
+        }
+
+        return nums1;
+    }
+}

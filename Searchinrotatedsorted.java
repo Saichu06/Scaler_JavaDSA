27e84907a8c606f898f8c417a0bd8f982bf41579
Saichu <saichuwork@gmail.com>
2026-03-12 10:30:20 +0530

    Correction util

--- Searchinrotatedsorted.java
+++ Searchinrotatedsorted.java
@@ -1,3 +1,4 @@
+import java.util.*;
 class Solution {
     public int search(int[] nums, int target) {
         int n=nums.length;
--- firstandlastindex.java
+++ firstandlastindex.java
@@ -1,3 +1,4 @@
+import java.util.*;
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         int left=0;
